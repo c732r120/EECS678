2ddc0018b1d481dd9686013db73d9499150cc5a3
c732r120 <chinmay.ratnaparkhi@gmail.com>
2015-03-04 21:10:12 -0600

    Added more functionality
    Functional features include :
    
    1. cd - sets working directory to HOME
    2. cd .. - sets working directory to the Parent folder
    3. cd / - sets working directory to /
    4. cd path - sets working directory to the provided path
    
    5. set - set HOME, PATH and ROOT values
    6. ls - lists files/folders in the current directory
    7. dir - lists files/folders in the current directory
    
    fork - runs an executable file (hardcoded — needs work)
    
    Code has been cleaned up. Console is cleared before and after the
    execution. Makefile has been added.

--- Project1/main.c
+++ Project1/main.c
@@ -4,88 +4,209 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+#include <dirent.h> 
 #include <pwd.h>
-
-
+#define clean() printf("\033[H\033[J")
 
 int main (int argc, char **argv, char **envp) 
 {
 
-	char cmd[256];
+	char cmd[1024];
+	char cmd2[1024]; // just to keep the original value of cmd intact.
 	char* home_dir;
 	char* pch;
+	char* first_command;
+	char* second_command;
+	char cwd[1024];
+	char charTotext[1024];
+	
+	//printf("%s", argv[1]);
+	//if(strcmp(argv[1], "<") == 0) printf("%s", argv[2]);
+	
+	
+	clean();
 	
 	while (true)
 	{
 		printf("> ");
-		fgets(cmd, sizeof cmd, stdin);
-		cmd[strlen(cmd)-1] = '\0';
 		
+		fgets(cmd, sizeof cmd, stdin);
+		cmd[strlen(cmd)-1] = '\0'; 	// make the last character '\0'
+		
+		strcpy(cmd2, cmd); // Keep the original value of cmd in cmd2 before the mutations
 		
 		pch = strtok(cmd, " ");
-		printf("begins");
-
+		int i=0;
 		while (pch != NULL)
   		{
-    		printf ("%s\n",pch);
+    		if(i==0)
+    		{
+    			first_command= pch;
+    		}
+    		if(i==1)
+    		{
+    			second_command = pch;
+    		}
+    		if(i > 1 && strcmp(first_command, "cd") ==0)
+    		{
+				strcpy(charTotext, pch);
+    			second_command = strcat(second_command, " ");
+    			second_command = strcat(second_command, charTotext);
+    		}
     		pch = strtok (NULL, " ");
+    		
+  		i++;	
   		}
-				printf("ends");
-		
-		
-		printf("%s", cmd);
-		if ((strcmp(cmd, "exit") == 0) || (strcmp(cmd, "quit") == 0))
+		else if(strcmp(first_command, "fork") == 0)
 		{
+			system("./a.out");
+		}
+		/* Command : where - Print the current location. */
+		else if ((strcmp(first_command, "where") == 0))
+		{
+			if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s\n\n", cwd);
+		}
+		else if ((strcmp(first_command, "exit") == 0) || (strcmp(first_command, "quit") == 0))
+		{
+			clean();
 			break;
 		}
-		if (strcmp(cmd, "cd") == 0)
+		else if (strcmp(first_command, "set")==0)
 		{
-			int checker = chdir(getenv("HOME"));
-			if(checker == 0 ) 
-			{	
-				printf("works!");
-				char *cwd;
-				cwd = getcwd (0, 0);
-				printf("%s", cwd);
-				
-				
-				
-  				int len;
-			   len = strlen(cwd);
-			   printf("%d", len);
-			}
-			else 
+			if(i == 2)
 			{
-				printf("problem!");
+				int ret;
+				char* setHelper;
+				char variable_name[1024];
+				char variable_value[1024];
+				char setter[1024];
+				int j=0;				
+				setHelper = strtok (second_command, "=");
+
+				while (setHelper != NULL)
+		  		{
+    				if(j==0)
+    				{
+	    				strcpy(variable_name, setHelper);
+    				}
+    				if( j==1)
+    				{
+		    			strcpy(variable_value, setHelper);
+    				}
+    				if(j > 1 )
+    				{
+    					printf("Error in the input. \n\n");
+		    		}
+    		
+			    	setHelper = strtok (NULL, "=");
+  					j++;	
+  				}
+  				
+				ret = setenv(variable_name, variable_value, 1);
+				if(ret == 0)
+				{
+					 printf("New %s has been set to %s \n\n", variable_name, variable_value);		
+				}else
+				{
+					 printf("Encountered error while setting %s value.\n\n", variable_name);		
+				}
+				
 			}
-		
+			else
+			{
+				printf("ERROR:\tInvalid number of arguments provided.\n");
+				printf("\tLegal use: set VARIABLE=VALUE  \n\n");
+			}
 		}
-		
-		if ((strcmp(cmd, "home asd\n") == 0))
-		{
-			printf("recognized\n");
-		  /*chdir(getenv("HOME"));
-		   printf("\n Now changed \n");
-		   		   if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s", cwd); */
-		   		    printf("PATH : %s\n", getenv("PATH"));
-					printf("HOME : %s\n", getenv("HOME"));
-					printf("ROOT : %s\n", getenv("ROOT"));
+		else if (strcmp(first_command, "cd") == 0)
+		{	
+			// Only one word is present i.e. 'cd'
+			if(i==1) 
+			{
+				int checker = chdir(getenv("HOME"));
+				if(checker == 0 ) 
+				{	
+					printf("Arrived at : HOME.\n\n");
+					char *cwd;
+					cwd = getcwd (0, 0);
+
+  					/*int len;
+			   		len = strlen(cwd);*/
+			   		
+				}
+				else 
+				{
+					printf("Problem loading the home directory path!\n\n");
+				}
+				
+			}
+			if(i>1)
+			{
+				
+				if(strncmp(second_command, "/", 1)==0)
+				{
+					chdir(second_command); 
+					printf("Arrived at : ");
+					if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s\n\n", cwd);
+				}
+				else if (strcmp(second_command, "..")==0)
+				{
+					chdir("..");
+					printf("Arrived at : ");
+					if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s\n\n", cwd);
+				}
+				else
+				{
+					char* changeTo;
+					if (getcwd(cwd, sizeof(cwd)) != NULL);
+					changeTo = strcat(cwd, "/");
+					changeTo = strcat(changeTo, second_command);
+					chdir(changeTo); 
+					
+					printf("Arrived at : ");
+					if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s\n\n", cwd);
+				
+				}
+				
+			}
 			
 		}
+		else if ((strcmp(cmd2, "get paths") == 0))
+		{
+			printf("PATH : %s\n", getenv("PATH"));
+			printf("HOME : %s\n", getenv("HOME"));
+			printf("ROOT : %s\n\n", getenv("ROOT"));
+			
+		}
+		else if ((strcmp(cmd2, "crap crap") == 0))
+		{
+			chdir("/Users/chinmay/Documents/KU/Spring 2015");
+			if (getcwd(cwd, sizeof(cwd)) != NULL) printf("%s\n\n", cwd);
+		}
+		else if(strcmp(first_command, "ls" )== 0 || strcmp(first_command, "dir" )== 0)
+		{
+			DIR	*d;
+			struct dirent *dir;
+			d = opendir(".");
+			if(d)
+			{
+				int count;
+				while ((dir = readdir(d)) != NULL)
+				{
+						printf("%s\n", dir->d_name);
+
+				}
+				printf("\n");
+				closedir(d);
+			}
 		
-		
+		}
+		else
+		{
+			printf("ERROR:\tcommand not recognized. \n\n");
+		}
 		
 	}
 
-	return 0;
+return 0;
 }
-
-
-/*if((home_dir = getenv("HOME")) == NULL)
-			{
-				printf("I'm here!");
-				home_dir = getpwuid(getuid())->pw_dir;
-			}
-			*/
-			//printf(" OKAY!! %s Done!", &home_dir);
