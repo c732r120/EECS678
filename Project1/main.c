ce4f9b279be2078b09e8d7f86c4bd0b810fff4ff
c732r120 <chinmay.ratnaparkhi@gmail.com>
2015-03-09 21:52:11 -0500

    execvpe --> execvp for OS X
    execvpe() does not work on OS X. Commented out the execvpe() calls and
    added new execvp() calls for OS X.
    
    Processes can be run in background.
    
    Example call —>
    > open -a textedit &

--- Project1/main.c
+++ Project1/main.c
@@ -291,8 +291,8 @@ int main(int argc, char * * argv, char * * envp) {
 
                         close(pipefd_1[0]);
                         close(pipefd_1[1]);
-
-                        if (execvpe(cmdbuf, argv, environ) < 0) {
+                        //if (execvpe(cmdbuf, argv, environ) < 0) {	//Linux
+                        if (execvp(cmdbuf, argv) < 0) {	// OS X
                             fprintf(stderr, "Error executing the first process. ERROR#%d\n", errno);
                             return EXIT_FAILURE;
                         }
@@ -308,8 +308,8 @@ int main(int argc, char * * argv, char * * envp) {
 
                         close(pipefd_1[0]);
                         close(pipefd_1[1]);
-
-                        if (execvpe(cmdbuf, argv, environ)  < 0) {
+                        if (execvp(cmdbuf, argv) < 0) { // OS X
+                      //  if (execvpe(cmdbuf, argv, environ)  < 0) { // Linux
                             fprintf(stderr, "Error executing the second process. ERROR#%d\n", errno);
                             return EXIT_FAILURE;
                         }
@@ -336,14 +336,16 @@ int main(int argc, char * * argv, char * * envp) {
                         }
                         if (strcmp(commands[i - 1], "&") == 0) {
                             commands[i - 1] = NULL;
-                            exists = execvpe(commands[0], commands, environ);
+                            exists = execvp(commands[0], commands);	// OS X
+                            //exists = execvpe(commands[0], commands, environ); // Linux
                             if (exists == -1) {
                                 printf("ERROR:\tcommand not recognized.\n");
                                 printf("errno: %s\n", strerror(errno));
                             }
                         } else {
                             commands[i] = NULL;
-                            exists = execvpe(commands[0], commands, environ);
+                            exists = execvp(commands[0], commands);  // OS X
+                            //exists = execvpe(commands[0], commands, environ); // Linux
                             if (exists == -1) {
                                 printf("ERROR:\tcommand not recognized.\n");
                                 printf("errno: %s\n", strerror(errno));
